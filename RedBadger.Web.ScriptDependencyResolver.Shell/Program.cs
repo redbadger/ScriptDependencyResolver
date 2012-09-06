fddfa9d9d912ef713756c6b67ff443d7e5772b03
David Wynne <david.wynne@red-badger.com>
2012-09-06 15:57:36 +0100

    Add YUICompressor so the Resolve.exe can now be executed with a -min switch to compress the output.

--- RedBadger.Web.ScriptDependencyResolver.Shell/Program.cs
+++ RedBadger.Web.ScriptDependencyResolver.Shell/Program.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using RedBadger.Web.ScriptDependencyResolver;
-using System.Text;
-
-namespace RedBadger.Web.ScriptDependencyResolver.Shell
+﻿namespace RedBadger.Web.ScriptDependencyResolver.Shell
 {
-    class Program
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Text;
+
+    using Yahoo.Yui.Compressor;
+
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void CurrentDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine(e.ExceptionObject);
+            Environment.Exit(1);
+        }
+
+        private static void Main(string[] args)
         {
             SetUnhandledExceptionHandler();
             if (args.Length != 3 && args.Length != 4)
@@ -19,40 +24,45 @@ namespace RedBadger.Web.ScriptDependencyResolver.Shell
                 return;
             }
 
-            var appRoot = args[0];
-            var scriptsPath = args[1];
-            var outputFile = args[2];
+            string appRoot = args[0];
+            string scriptsPath = args[1];
+            string outputFile = args[2];
 
-            WriteOutput(appRoot, scriptsPath, outputFile);
+            bool minifyOutput = args.Length > 3 && args[3] == "-min";
+
+            WriteOutput(appRoot, scriptsPath, outputFile, minifyOutput);
         }
 
         private static void PrintUsage()
         {
-            Console.WriteLine("Usage: {0} ApplicationRoot ScriptPath Output", Assembly.GetExecutingAssembly().GetName().Name);
+            Console.WriteLine(
+                "Usage: {0} ApplicationRoot ScriptPath Output", Assembly.GetExecutingAssembly().GetName().Name);
         }
 
         private static void SetUnhandledExceptionHandler()
         {
-            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainUnhandledException;
         }
 
-        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        private static void WriteOutput(string appRoot, string scriptsPath, string outputFile, bool minifyOutput)
         {
-            Console.WriteLine(e.ExceptionObject);
-            Environment.Exit(1);
-        }
+            var stringBuilder = new StringBuilder();
 
-        private static void WriteOutput(string appRoot, string scriptsPath, string outputFile)
-        {            
-            using (var writer = new StreamWriter(outputFile))
+            foreach (string file in new Resolver(appRoot, scriptsPath, "*.js").Resolve())
             {
-                foreach (string file in new Resolver(appRoot, scriptsPath, "*.js").Resolve())
-                {
-                    Console.WriteLine("Appending {0}", file);
-                    writer.WriteLine("// ~" + file.ToLower().Replace(scriptsPath.ToLower(), "").Replace('\\', '/'));
-                    writer.WriteLine(File.ReadAllText(file));
-                }                
+                Console.WriteLine("Appending {0}", file);
+                stringBuilder.AppendLine(
+                    "// ~" + file.ToLower().Replace(scriptsPath.ToLower(), string.Empty).Replace('\\', '/'));
+                stringBuilder.AppendLine(File.ReadAllText(file));
             }
+
+            using (Stream output = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
+            {
+                var writer = new StreamWriter(output);
+                writer.WriteLine(minifyOutput ? new JavaScriptCompressor().Compress(stringBuilder.ToString()) : stringBuilder.ToString());
+                writer.Flush();
+            }
+
             Console.WriteLine("Written to {0}.", outputFile);
         }
     }
